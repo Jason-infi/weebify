0d1255c86e2c4d8eff7078396ef2942df1a9eff5
Jason-infi <teamftwjason@gmail.com>
2023-01-02 15:31:13 +0530

    fixing eslint errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,13 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-import ThemeContextWrapper from "../utilities/ThemeContextWrapper";
 import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <ThemeContextWrapper>
-        <RecoilRoot>
-          <Component {...pageProps} />
-        </RecoilRoot>
-      </ThemeContextWrapper>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
     </SessionProvider>
   );
 }
